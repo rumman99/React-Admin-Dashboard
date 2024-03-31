96c1385e723d8f8ea3ecce8bbbba43ccc6b5b8b4
rumman99 <tasnimrumman369@gmail.com>
2024-03-31 15:40:50 +0600

    all chart completed

--- server/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const dbConnection= async()=>{
-    return await mongoose.connect(process.env.DATABASE_URL)
-    .then(()=> console.log("Database Connected"))
-    .catch((err)=> console.log(err));
-}
-
-export default dbConnection;
